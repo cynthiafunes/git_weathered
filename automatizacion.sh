dd9744423793eedc8d3cd708b252c9ed18a9b4ee
cynthiafunes <funescy@gmail.com>
2024-09-17 18:13:22 -0400

    Agregue el script bash de automatizacion

---
+++ automatizacion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. Activar el entorno virtual
+echo "Activando el entorno virtual..."
+source .venv/Scripts/activate
+
+# 2. Instalar dependencias
+echo "Instalando dependencias..."
+pip install -r requirements.txt 
+
+# 3. Ejecutar la aplicación CLI
+echo "Ejecutando la aplicacion CLI..."
+python cli.py Madrid json
+
+# 4. Mensaje final
+echo "Ejecución completada con éxito."
