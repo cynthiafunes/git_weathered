33fb8310850a74423b66aaeda626eb94925af238
cynthiafunes <funescy@gmail.com>
2024-09-17 18:22:13 -0400

    Agregue el manejo de errores para solicitudes fallidas y ubicaciones no encontradas

--- api.py
+++ api.py
@@ -1,29 +1,40 @@
 import requests
+import requests.exceptions
 from api_key import api_key
+
 
 def obtener_datos_clima(ciudad):
     url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ciudad}&lang=es"
-    respuesta = requests.get(url)
 
-    if respuesta.status_code == 200:
-        json_data = respuesta.json()
-        
-        l = json_data['location']
-        c = json_data['current']
+    try:
+        respuesta = requests.get(url)
 
-        return{
-            "ciudad": l['name'],
-            "region": l['region'],
-            "pais": l['country'],
-            "temperatura_actual": c['temp_c'],
-            "condicion_actual": c['condition']['text'],
-            "precipitacion": c['precip_mm'],
-            "humedad": c['humidity'],
-            "fecha_actualizacion": c['last_updated'],
-        }
-    else:
-        raise Exception(f"Error: {respuesta.status_code}")
-    
+        if respuesta.status_code == 200:
+            json_data = respuesta.json()
+
+            if "error" in json_data:
+                raise Exception(f"Ubicacion no encontrada: {ciudad}")
+
+            location = json_data["location"]
+            c = json_data["current"]
+
+            return {
+                "ciudad": location["name"],
+                "region": location["region"],
+                "pais": location["country"],
+                "temperatura_actual": c["temp_c"],
+                "condicion_actual": c["condition"]["text"],
+                "precipitacion": c["precip_mm"],
+                "humedad": c["humidity"],
+                "fecha_actualizacion": c["last_updated"],
+            }
+        else:
+            raise Exception(f"Error: {respuesta.status_code}")
+
+    except requests.exceptions.ConnectionError:
+        raise Exception("Error de conexion")
+
+
 if __name__ == "__main__":
     ciudad_prueba = input("Ingrese la ciudad para probar la API: ")
     try:
@@ -31,4 +42,3 @@ if __name__ == "__main__":
         print(datos)
     except Exception as e:
         print(f"Error: {e}")
- 
--- cli.py
+++ cli.py
@@ -4,26 +4,35 @@ import csv
 import sys
 from api import obtener_datos_clima
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Cli para la obtencion de datos climaticos")
+    parser = argparse.ArgumentParser(
+        description="Cli para la obtencion de datos climaticos"
+    )
     parser.add_argument("ciudad", help="Nombre de la ciudad")
-    parser.add_argument("formato", choices=["json", "csv"], help="Formato de salida (json o csv)")
-    
+    parser.add_argument(
+        "formato", choices=["json", "csv"], help="Formato de salida (json o csv)"
+    )
+
     args = parser.parse_args()
-    
+
     ciudad = args.ciudad
     formato_salida = args.formato
-    
+
     print(f"Obtener el clima de {ciudad} en {formato_salida}")
 
-    datos_clima = obtener_datos_clima(ciudad)
-    
-    if formato_salida == "json":
-        print(json.dumps(datos_clima, indent=2))
-    else:  
-        writer = csv.DictWriter(sys.stdout, fieldnames=datos_clima.keys())
-        writer.writeheader()
-        writer.writerow(datos_clima)
+    try:
+        datos_clima = obtener_datos_clima(ciudad)
+
+        if formato_salida == "json":
+            print(json.dumps(datos_clima, indent=2))
+        else:
+            writer = csv.DictWriter(sys.stdout, fieldnames=datos_clima.keys())
+            writer.writeheader()
+            writer.writerow(datos_clima)
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     main()
