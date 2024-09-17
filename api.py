2fc33ad8b174786c58abb1bbb3b218b1aeb2414a
cynthiafunes <funescy@gmail.com>
2024-09-17 16:46:43 -0400

    Modificaciones al script de la api

--- api.py
+++ api.py
@@ -1,51 +1,34 @@
 import requests
-import csv
-import json
 from api_key import api_key
 
-ciudad = input("Ingrese la Ciudad: ") 
+def obtener_datos_clima(ciudad):
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ciudad}&lang=es"
+    respuesta = requests.get(url)
 
-url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ciudad}&lang=es"
-
-respuesta = requests.get(url)
-
-if respuesta.status_code == 200:
-    json_data = respuesta.json()
-    
-    l = json_data['location']
-    c = json_data['current']
-    
-    ciudad = l['name']
-    region = l['region']
-    pais = l['country']
-    temperatura_actual = c['temp_c']
-    condicion_actual = c['condition']['text']
-    precipitacion = c['precip_mm']
-    humedad = c['humidity']
-    fecha_actualizacion = c['last_updated']
-    
-    formato = input("formato deseas los datos? (json/csv): ")
-    
-    if formato == "json":
-        datos_resu = {
-            "ciudad": ciudad,
-            "region": region,
-            "pais": pais,
-            "temperatura_actual": temperatura_actual,
-            "condicion_actual": condicion_actual,
-            "precipitacion": precipitacion,
-            "humedad": humedad,
-            "fecha_actualizacion": fecha_actualizacion,
-        }
-        print(json.dumps(datos_resu, indent=4))
+    if respuesta.status_code == 200:
+        json_data = respuesta.json()
         
-    elif formato == "csv":
-        with open('datos_clima.csv', mode='w', newline='', encoding='utf-8') as archivo_csv:
-            writer = csv.writer(archivo_csv)
-            writer.writerow(["ciudad", "region", "pais", "temperatura_actual", "condicion_actual", "precipitacion", "humedad", "fecha_actualizacion"])
-            writer.writerow([ciudad, region, pais, temperatura_actual, condicion_actual, precipitacion, humedad, fecha_actualizacion])
-        print("Datos guardados en 'datos_clima.csv'")
+        l = json_data['location']
+        c = json_data['current']
+
+        return{
+            "ciudad": l['name'],
+            "region": l['region'],
+            "pais": l['country'],
+            "temperatura_actual": c['temp_c'],
+            "condicion_actual": c['condition']['text'],
+            "precipitacion": c['precip_mm'],
+            "humedad": c['humidity'],
+            "fecha_actualizacion": c['last_updated'],
+        }
     else:
-        print("Formato no valido, elige json o csv")
-else:
-    print(f"Error: {respuesta.status_code}")
+        raise Exception(f"Error: {respuesta.status_code}")
+    
+if __name__ == "__main__":
+    ciudad_prueba = input("Ingrese la ciudad para probar la API: ")
+    try:
+        datos = obtener_datos_clima(ciudad_prueba)
+        print(datos)
+    except Exception as e:
+        print(f"Error: {e}")
+ 
