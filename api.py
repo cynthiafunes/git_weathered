b995e1653287ee3bd2908004edf8839b69885a10
cynthiafunes <funescy@gmail.com>
2024-09-19 09:04:21 -0400

    Modificaciones

--- api.py
+++ api.py
@@ -4,26 +4,28 @@ from api_key import api_key
 
 
 def obtener_datos_clima(ciudad):
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ciudad}&lang=es"
+    base_url = "http://api.weatherapi.com/v1/current.json?"
+    params = {"key": api_key, "q": ciudad, "lang": "es"}
 
     try:
-        respuesta = requests.get(url)
+        respuesta = requests.get(base_url, params=params)
+        print(f"{respuesta.url}")
 
         if respuesta.status_code == 200:
             json_data = respuesta.json()
 
             location = json_data["location"]
-            c = json_data["current"]
+            current = json_data["current"]
 
             return {
                 "ciudad": location["name"],
                 "region": location["region"],
                 "pais": location["country"],
-                "temperatura_actual": c["temp_c"],
-                "condicion_actual": c["condition"]["text"],
-                "precipitacion": c["precip_mm"],
-                "humedad": c["humidity"],
-                "fecha_actualizacion": c["last_updated"],
+                "temperatura_actual": current["temp_c"],
+                "condicion_actual": current["condition"]["text"],
+                "precipitacion": current["precip_mm"],
+                "humedad": current["humidity"],
+                "fecha_actualizacion": current["last_updated"],
             }
 
         elif respuesta.status_code == 400:
@@ -33,12 +35,3 @@ def obtener_datos_clima(ciudad):
 
     except requests.exceptions.ConnectionError:
         raise Exception("Error de conexion")
-
-
-if __name__ == "__main__":
-    ciudad_prueba = input("Ingrese la ciudad para probar la API: ")
-    try:
-        datos = obtener_datos_clima(ciudad_prueba)
-        print(datos)
-    except Exception as e:
-        print(f"Error: {e}")
